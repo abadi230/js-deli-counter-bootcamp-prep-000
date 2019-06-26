64e17ce9f532f7cd1281aa5e32de24e7c43f8081
Abdullah Bajaman <ambajaman@gmail.com>
2019-06-26 17:25:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,33 +1,73 @@
-var katzDeliLine = [];
+var arr;
 function takeANumber(line, name){
   line.push(name);
-  // console.log(`Welcome, ${name}. You are number ${line.length} in line.`);
-  return `Welcome, ${name}. You are number ${line.length} in line.`;
+  return `Welcome, ${name}. You are number ${line.length} in line.`
 }
 
-function nowServing(katzDeliLine){
-  // var firstP = katzDeliLine.length - (katzDeliLine.length - 1);
+function nowServing(line){
+  if(!line.length) return `There is nobody waiting to be served!`  
+  if(line.length) return `${line.shift()}`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var katzDeliLine = [];
+// function takeANumber(line, name){
+//   line.push(name);
+//   // console.log(`Welcome, ${name}. You are number ${line.length} in line.`);
+//   return `Welcome, ${name}. You are number ${line.length} in line.`;
+// }
+
+// function nowServing(katzDeliLine){
+//   // var firstP = katzDeliLine.length - (katzDeliLine.length - 1);
   
-  if (!katzDeliLine.length) {
+//   if (!katzDeliLine.length) {
     
-    return 'There is nobody waiting to be served!';
+//     return 'There is nobody waiting to be served!';
     
-  } else {
+//   } else {
   
-    return `Currently serving ${katzDeliLine.shift()}.` // print katzDeliLine[0] then remove it
-  }
+//     return `Currently serving ${katzDeliLine.shift()}.` // print katzDeliLine[0] then remove it
+//   }
  
-}
+// }
 
-function currentLine(line){
-  if(!line.length){
-    return `The line is currently empty.`;
-  } else {
-    var numOfName = [];
-    for(var i=0; i < line.length; i++){
-      numOfName.push(`${i+1}. ${line[i]}`)
-    }
+// function currentLine(line){
+//   if(!line.length){
+//     return `The line is currently empty.`;
+//   } else {
+//     var numOfName = [];
+//     for(var i=0; i < line.length; i++){
+//       numOfName.push(`${i+1}. ${line[i]}`)
+//     }
     
-    return `The line is currently: ${numOfName.join(', ')}`;
-  }
-}
+//     return `The line is currently: ${numOfName.join(', ')}`;
+//   }
+  
+// }
+
